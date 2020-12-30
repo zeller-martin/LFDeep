9c3b9dec9206e1cf66e68b14f6e7a0efd96ab883
zeller-martin <54265706+zeller-martin@users.noreply.github.com>
2020-12-30 12:24:04 +0100

    Update layers.py

--- deep_loop/layers.py
+++ deep_loop/layers.py
@@ -1,5 +1,34 @@
+import tensorflow as tf
+from tensorflow import keras
 
 
+    
+class ZscoreTimeseries(keras.layers.Layer):
+    def __init__(self):
+        super(ZscoreTimeseries, self).__init__()
+        self.std = None
+        
+    def call(self, inputs):
+        self.std =  ...
+        zscored = (inputs - mean )/ std
+        return zscored
+
+class AngularOutput(keras.layers.Layer):
+    def __init__(self):
+        super(AngularOutput, self).__init__()
+    
+    def call(self, inputs):
+        return tf.atan2
+    
+class AmplitudeRescalingOutput(keras.layers.Layer):
+    def __init__(self, reference_layer):
+        super(AngularOutput, self).__init__()
+        self.scaling_factors = reference_layer.std
+        
+    def call(self, inputs):
+        return inputs * self.scaling_factors
+    
+    
 ## z score preprocessing layer
 
 ## arctan2 layer
