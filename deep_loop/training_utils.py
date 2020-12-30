155215814061c577127b131e237659917c964692
zeller-martin <54265706+zeller-martin@users.noreply.github.com>
2020-12-30 20:05:21 +0100

    Update training_utils.py

--- deep_loop/training_utils.py
+++ deep_loop/training_utils.py
@@ -1,7 +1,7 @@
-
-
-### circular loss
-
+def circular_loss(y_true, y_pred):
+    y_true = tf.cast(tf.reshape(y_true, [-1]), tf.float32)
+    return 1 - tf.cos(angle - y_true)
+  
 ## training_dataset function
 
 ### data generators
