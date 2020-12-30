8d1b46787dcd1dca8650b17819a15d4db9977423
zeller-martin <54265706+zeller-martin@users.noreply.github.com>
2020-12-30 20:31:12 +0100

    Update training_utils.py

--- deep_loop/training_utils.py
+++ deep_loop/training_utils.py
@@ -1,3 +1,27 @@
+import time
+
+
+
+
+
+def create_training_files(files, file_handler, bandpass_f, channels = None, order = 4, filename = None):
+    
+    if filename is None:
+        
+    
+    
+
+    
+class BasicFileHandler:
+    
+    def __init__(self, fs, order = 'row', dtype = np.int16):
+        self.fs = fs
+        self.order = order
+        self.dtype = dtype
+        
+    def __call__(self, file, 
+
+
 def circular_loss(y_true, y_pred):
     y_true = tf.cast(tf.reshape(y_true, [-1]), tf.float32)
     return 1 - tf.cos(angle - y_true)
