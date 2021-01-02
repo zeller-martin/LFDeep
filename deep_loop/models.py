aa99011f38012992d392d7f8f32a8aaa649e2caa
zeller-martin <54265706+zeller-martin@users.noreply.github.com>
2021-01-02 11:38:21 +0100

    Update models.py

--- deep_loop/models.py
+++ deep_loop/models.py
@@ -1,1 +1,23 @@
+import tensorflow as tf
+from tensorflow import keras
+from tf.keras import layers
 
+_default_layers = [
+  
+]
+
+
+
+def create_phase_model(input_shape = 1024, middle_layers = _default_layers ):
+    layers = [layers.Input,
+              Zscore_Timeseries ...]
+    
+    layers.extend(middle_layers)
+    
+    layers.extend([layers.Dense(2),
+                  AngularOutput])
+                  
+    model = keras.Sequential(layers)
+    return model
+    
+def create_amplitude_model(input_shape = 1024, middle_layers = _default_layers ):
