3efe2eb278bb4353d028d6c27cd42dfe21b4bcfb
zeller-martin <54265706+zeller-martin@users.noreply.github.com>
2021-01-02 11:12:21 +0100

    Update training_utils.py

--- deep_loop/training_utils.py
+++ deep_loop/training_utils.py
@@ -1,15 +1,20 @@
 import time
 
+def _expand_to_iterable(x, n_iter):
+    try:
+        iter(x)
+        return x
+    except TypeError:
+        out = [x for i in range(n_iter)]
+        return out
 
 
 
-
-def create_training_files(files, file_handler, bandpass_f, channels = None, order = 4, filename = None):
+def create_training_files(files, file_handler, bandpass_f, channel, n_channels, order = 4, filename = None):
     
     if filename is None:
         
-    
-    
+
 
     
 class BasicFileHandler:
@@ -19,7 +24,25 @@ class BasicFileHandler:
         self.order = order
         self.dtype = dtype
         
-    def __call__(self, file, 
+    def __call__(self, file, band, channel, n_channels):
+        sos = signal.butter(self.order, band, 'bandpass', fs = self.fs, output = 'sos')
+        
+        dat_map = np.memmap(file, dtype = np.int16)
+        
+        if self.order = 'row':
+            target_shape = (dat_map.shape[0] // n_channels, n_channels)
+            dat_map.resize( target_shape )
+        else:
+            target_shape = (n_channels, dat_map.shape[0] // n_channels)
+            dat_map.resize( target_shape )
+            dat_map = dat_map.T
+            
+        data = np.array(dat_map[:, channel])
+        
+        
+        
+        
+        
 
 
 def circular_loss(y_true, y_pred):
