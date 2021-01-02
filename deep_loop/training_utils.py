d954fabe6931968761b61e7ac152b2e5080116b4
zeller-martin <54265706+zeller-martin@users.noreply.github.com>
2021-01-02 11:43:11 +0100

    Update training_utils.py

--- deep_loop/training_utils.py
+++ deep_loop/training_utils.py
@@ -47,7 +47,7 @@ class BasicFileHandler:
 
 def circular_loss(y_true, y_pred):
     y_true = tf.cast(tf.reshape(y_true, [-1]), tf.float32)
-    return 1 - tf.cos(angle - y_true)
+    return tf.sqrt(1 - tf.cos(angle - y_true))
   
 ## training_dataset function
 
