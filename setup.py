76ba78653c9065cc062cb158bd1e34b25fa6d869
zeller-martin <54265706+zeller-martin@users.noreply.github.com>
2021-01-08 14:01:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
 
+from setuptools import setup, find_packages
+
+setup(
+    name='deep_loop',
+    version='0.1',
+    description='Tools for ephys analysis',
+    author='Martin Zeller',
+    author_email='martin.zeller@fau.de',
+    url='https://github.com/zeller-martin/deep_loop',
+    packages=find_packages(exclude=('tests', 'docs'))
+)
