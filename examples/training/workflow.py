01340052caa5d4964b99334bcac568adff5eedac
zeller-martin <54265706+zeller-martin@users.noreply.github.com>
2021-01-08 15:25:46 +0100

    Update workflow.py

--- examples/training/workflow.py
+++ examples/training/workflow.py
@@ -7,7 +7,7 @@ batches_per_epoch = 128
 size = 1024
 
 x_files = glob.glob('data/*_raw.float32')
-y_files = glob.glob('data/*_phase.float32')
+y_files = glob.glob('data/*_theta_phase.float32')
                     
 x_train, y_train, x_val, y_val = deep_loop.split_data(x_files, y_files)
 
