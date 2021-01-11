8cd05bdd7a3c9808d69bf5ea5aee4086d4b28b74
zeller-martin <54265706+zeller-martin@users.noreply.github.com>
2021-01-11 20:31:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ from setuptools import setup, find_packages
 setup(
     name='LFDeep',
     version='0.1',
-    description='Tools for ephys analysis',
+    description='Deep learning for predicting instantaneous band-limited amplitude and phase from broadband signals.',
     author='Martin Zeller',
     author_email='martin.zeller@fau.de',
-    url='https://github.com/zeller-martin/deep_loop',
-    packages=find_packages(exclude=('tests', 'docs'))
+    url='https://github.com/zeller-martin/LFDeep',
+    packages=find_packages(exclude=('examples')),
+    install_requires=['tensorflow', 'numpy', 'scipy', 'matplotlib'],
 )
