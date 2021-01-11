89eda8a9bb3660291868f73a00d5a9b64bb01862
zeller-martin <54265706+zeller-martin@users.noreply.github.com>
2021-01-11 13:28:55 +0100

    Update and rename helpers.py to _helpers.py

--- examples/create_training_data/_helpers.py
+++ examples/create_training_data/_helpers.py
@@ -1,18 +1,32 @@
 import numpy as np
 from scipy import signal
 
+
+
 def read_binary(src_file,
                 n_channels = 1,
                 selected_channel = 0,
-                src_precision = np.int16):
+                src_precision = np.int16,
+                src_order = 'row'):
+    data = np.fromfile(src_file, dtype = src_precision)
+    
+    if src_order == 'row':
+        data.resize(data.shape[0] // n_channels, n_channels)
+        x = data[:, selected_channel]
+    elif src_order in ('col', 'column'):
+        data.resize(n_channels, data.shape[0] // n_channels)
+        x = data[selected_channel, :]
+    else:
+        raise ValueError(f'src_order not understood - "row", "col" and "column" permissible, given was "{src_order}".')
  
+    return x
 
-def bandpass_filter(y, corner_frequencies, sampling_rate):
+def bandpass_filter(x, corner_frequencies, sampling_rate):
     sos = signal.butter(4, corner_frequencies, 'bandpass', fs = sampling_rate, output = 'sos')
-    return signal.sosfiltfilt(sos, y)
+    return signal.sosfiltfilt(sos, x)
 
-def hilbert_phase(y):
-    return np.angle(signal.hilbert(y))
+def hilbert_phase(x):
+    return np.angle(signal.hilbert(x))
     
 def _make_gauss_kernel(dt, sigma, width = 5):
     n_timesteps = 2 * width * sigma / dt + 1
@@ -22,7 +36,7 @@ def _make_gauss_kernel(dt, sigma, width = 5):
     y /= np.sum(y)
     return y
 
-def rms_amplitude(y,
+def rms_amplitude(x,
                   sampling_rate = None,
                   kernel_sd_seconds = None):
     
@@ -30,4 +44,4 @@ def rms_amplitude(y,
         raise ValueError('sampling_rate and kernel_sd_seconds have to be specified!')
 
     kernel = _make_gauss_kernel(1 / sampling_rate, kernel_sd_seconds)
-    return signal.oaconvolve(y**2, kernel, mode = 'same')**.5
+    return signal.oaconvolve(x**2, kernel, mode = 'same')**.5
