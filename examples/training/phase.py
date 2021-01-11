e3ed15c1594c43ef43a4ee2cfe2677f85034a5b2
zeller-martin <martin.zeller@fau.de>
2021-01-11 13:18:46 +0100

    update

---
+++ examples/training/phase.py
@@ -0,0 +1,36 @@
+import deep_loop
+import glob
+from IPython import embed
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+batch_size = 8192
+batches_per_epoch = 32
+size = 1024
+
+x_files = glob.glob('data/*CA1*_raw.float32')
+y_files = glob.glob('data/*CA1*_theta_phase.float32')
+             
+
+x_train, y_train, x_val, y_val = deep_loop.split_data(x_files, y_files)
+
+training_generator = deep_loop.DeepLoopGenerator(x_train, y_train, batch_size, batches_per_epoch, size)
+validation_generator = deep_loop.DeepLoopGenerator(x_val, y_val, batch_size, batches_per_epoch, size)
+
+
+model = deep_loop.create_phase_model()
+
+model.summary()
+
+model.fit(training_generator,
+          validation_data = validation_generator,
+          validation_steps = 32,
+          steps_per_epoch = batches_per_epoch,
+          epochs = 2,
+          verbose = 1)
+
+deep_loop.evaluate_phase_model(model, validation_generator)
+
+
+embed()
