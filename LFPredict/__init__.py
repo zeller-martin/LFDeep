678728989897f43adce621eafc2b83d0af350481
zeller-martin <54265706+zeller-martin@users.noreply.github.com>
2021-01-28 13:21:16 +0100

    Update __init__.py

--- LFPredict/__init__.py
+++ LFPredict/__init__.py
@@ -1,2 +1,3 @@
 from .training_utils import DataGenerator, split_data, evaluate_phase_model, evaluate_amplitude_model, evaluate_joint_model
 from .models import create_amplitude_model, create_phase_model, create_joint_model, load_model
+from .benchmark import estimate_phase_hilbert
